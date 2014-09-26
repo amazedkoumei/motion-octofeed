2cdb422f78109338a55995d5844246e9ca477819
amazedkoumei <amazed.koumei@gmail.com>
2014-09-26 16:00:04 +0900

    remove unused file

--- app/feature_profile_view_controller.rb
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-class FeatureProfileViewController < FeatureTemplateWebViewController
-  
-  # url is "https://github.com/:username"
-  attr_accessor :url, :navTitle, :hideDoneButton  
-  def javaScript
-    "$('.avatared')[0].innerHTML"
-  end
-end
